b388444733a3d1bfa66e88709ff5b486c5274122
Виноградов Владимир Владимирович <vvvinogradov@gid.ru>
2025-06-25 17:02:15 +0300

    fix: фикс выравнивания служебного текста

--- src/modules/Gallery/components/ImageList/ImageList.tsx
+++ src/modules/Gallery/components/ImageList/ImageList.tsx
@@ -42,11 +42,15 @@ export const ImageList = ({
   return (
     <div
       className={classNames(
-        "mx-auto flex max-w-[1424px] flex-wrap justify-center gap-2 p-[16px] pb-[16px] lg:px-[80px] lg:pt-[40px]",
+        "mx-auto flex max-w-[1424px] flex-wrap gap-2 p-[16px] pb-[16px] lg:px-[80px] lg:pt-[40px]",
+        !noData && "justify-center",
         className,
       )}
       {...rest}
     >
+      <p className="text-gray-shade-78">
+        К сожалению, поиск не дал результатов
+      </p>
       {noData ? (
         <p className="text-gray-shade-78">
           К сожалению, поиск не дал результатов
